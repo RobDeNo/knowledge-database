48050e1a38b8f6fe7a16dcceeebf743123f3967a
RobyDeNo <robby2992@gmail.com>
2023-06-01 13:45:49 -0400

    ettt

--- networking/python-code/ip-raw-input.py
+++ networking/python-code/ip-raw-input.py
@@ -18,27 +18,29 @@ except socket.error as msg:
 # IPPROTO_IP creates a socket that sends/receives raw data for IPv4-based protocols (TCP, UDP, etc). It will handle the IP headers for you, but you are responsible for processing/creating additional protocol data inside the IP payload.
 # IPPROTO_RAW creates a socket that sends/receives raw data for any kind of protocol. It will not handle any headers for you, you are responsible for processing/creating all payload data, including IP and additional headers. (link)
 packet = ''
-src_ip = "127.0.0.1" 
-dst_ip = "127.0.0.1" 
+src_ip = "10.10.0.40" 
+dst_ip = "172.16.82.106" 
 
 ##################
 ##Build Packet Header##
 ##################
 # Lets add the IPv4 header information
 # This is normally 0x45 or 69 for Version and Internet Header Length
-ip_ver_ihl =
+ip_ver_ihl =0x45
 # This combines the DSCP and ECN feilds.  Type of service/QoS
-ip_tos =
+#DSCP = 26
+#ECN = 3
+ip_tos = 107
 # The kernel will fill in the actually length of the packet
 ip_len = 0
 # This sets the IP Identification for the packet. 1-65535
-ip_id =
+ip_id =  1
 # This sets the RES/DF/MF flags and fragmentation offset
-ip_frag =
+ip_frag = 0xCF0F
 # This determines the TTL of the packet when leaving the machine. 1-255
-ip_ttl =
+ip_ttl = 128
 # This sets the IP protocol to 16 (CHAOS) (reference IANA) Any other protocol it will expect additional headers to be created.
-ip_proto =
+ip_proto = 16
 # The kernel will fill in the checksum for the packet
 ip_check = 0
 # inet_aton(string) will convert an IP address to a 32 bit binary number
@@ -55,7 +57,7 @@ ip_header = pack('!BBHHHBBH4s4s' , ip_ver_ihl, ip_tos, ip_len, ip_id, ip_frag, i
 ##Message##
 ##########
 # Your custom protocol fields or data. We are going to just insert data here. Add your message where the "?" is. Ensure you obfuscate it though...don't want any clear text messages being spotted! You can encode with various data encodings. Base64, binascii
-message = b'last_name'                  #This should be the student's last name per the prompt
+message = b'DeNovellis'                  #This should be the student's last name per the prompt
 hidden_msg = binascii.hexlify(message)  #Students can choose which encodeing they want to use.
 # final packet creation
 packet = ip_header + hidden_msg
