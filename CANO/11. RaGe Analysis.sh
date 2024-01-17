28a14c3e5023c85a655bc81226cb7129eb2ca421
Robert <robby2992@gmail.com>
2024-01-17 11:24:01 -0500

    swqs

---
+++ CANO/11. RaGe Analysis.sh
@@ -0,0 +1,12 @@
+#Rage is rovers passive location analysis tool
+    3live colection and post mission
+#Setting up rage
+Emitter -> Rage Options
+    [] 802.11
+        X - Use Crop Circles
+        X - Use Detection Points
+        X - Use Measurement Report
+        X - Use AP Responses
+Tools -> Preferences
+    [] Targets
+#Rage KEY FOR PLAYBACK AND MISSION PLANNING
